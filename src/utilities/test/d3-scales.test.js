af1c8f705d39198f84311e5d4b33d5a9e880925c
cpeterson <cpeterson@collineargroup.com>
2020-04-16 08:48:22 -0700

    completes unit testing for d3-scales

--- src/utilities/test/d3-scales.test.js
+++ src/utilities/test/d3-scales.test.js
@@ -54,15 +54,27 @@ describe('Band Scale', () => {
     it('Should return a function when given valid arguments', () => {
         expect(typeof getBandScale(['a', 'b', 'c'], [0, 100])).toBe('function')
     })
-    // it('Resulting band scale should return  if provided domain[0]', () => {
-    //     expect(getBandScale(['a', 'b', 'c'], [0, 100])('a')).toBe(1)
-    // })
-    // it('Resulting band scale should return max if provided domain[domain.length - 1]', () => {
-    //     expect(getBandScale(['a', 'b', 'c'], [0, 100])('c')).toBe(99)
-    // })
-    // it('Resulting band scale should return mid if provided domain median', () => {
-    //     expect(getBandScale(['a', 'b', 'c'], [0, 100])('b')).toBe(50)
-    // })
+    it('Provided valid inputs, resulting band scale should have a method (bandwidth) that returns an integer within the provided range', () => {
+        const rangeMin = 0
+        const rangeMax = 100
+        const bandwidth = getBandScale(
+            ['a', 'b', 'c'],
+            [rangeMin, rangeMax]
+        ).bandwidth()
+        expect(bandwidth).toBeGreaterThan(rangeMin)
+        expect(bandwidth).toBeLessThan(rangeMax)
+    })
+    it('Resulting band scale should return a number within the range if provided a valid index of domain', () => {
+        const rangeMin = 0
+        const rangeMax = 100
+        const value = getBandScale(['a', 'b', 'c'], [rangeMin, rangeMax])('a')
+        expect(value).toBeGreaterThanOrEqual(rangeMin)
+        expect(value).toBeLessThanOrEqual(rangeMax)
+    })
+    it('Resulting band scale should return undefined if provided an invalid arg (not contained within domain)', () => {
+        const value = getBandScale(['a', 'b', 'c'], [0, 100])('e')
+        expect(value).toBe(undefined)
+    })
 })
 
 describe('Point Scale', () => {
@@ -77,5 +89,24 @@ describe('Point Scale', () => {
     })
     it('Should throw an error if range is not an array', () => {
         expect(() => getScalePoint([], {})).toThrowError(AssertionError)
+    })
+    it('Should not throw an error when given valid arguments', () => {
+        expect(() =>
+            getScalePoint(['a', 'b', 'c'], [0, 100])
+        ).not.toThrowError()
+    })
+    it('Should return a function when given valid arguments', () => {
+        expect(typeof getScalePoint(['a', 'b', 'c'], [0, 100])).toBe('function')
+    })
+    it('Resulting point scale should return a number within the range if provided a valid index of domain', () => {
+        const rangeMin = 0
+        const rangeMax = 100
+        const value = getScalePoint(['a', 'b', 'c'], [rangeMin, rangeMax])('a')
+        expect(value).toBeGreaterThanOrEqual(rangeMin)
+        expect(value).toBeLessThanOrEqual(rangeMax)
+    })
+    it('Resulting point scale should return undefined if provided an invalid arg (not contained within domain)', () => {
+        const value = getScalePoint(['a', 'b', 'c'], [0, 100])('e')
+        expect(value).toBe(undefined)
     })
 })

d2c9f279407173a5351b56a4816f3bb4d3e5b03b
cpeterson <cpeterson@collineargroup.com>
2020-04-17 11:03:12 -0700

    first file converted to ts

--- src/utilities/assersions.ts
+++ src/utilities/assersions.ts
@@ -1,6 +1,6 @@
-const assert = require('assert')
+import assert from 'assert'
 
-export const assertList = (...assertions) =>
+export const assertList = (...assertions: [boolean, string][]) =>
     assertions.forEach(([assertion, errMessage]) =>
         assert(assertion, errMessage)
     )

ef491455cf59f3f41a2939cd7459499913f0603a
cpeterson <cpeterson@collineargroup.com>
2020-04-17 11:29:20 -0700

    converts d3-scales to ts

--- src/utilities/d3-scales.ts
+++ src/utilities/d3-scales.ts
@@ -1,21 +1,25 @@
 import { scaleLinear, scalePoint, scaleBand } from 'd3'
 import { assertList } from './assersions'
 
-const getScalePoint = (fullDomain, range, padding = 0) => {
+const getScalePoint = (
+    fullDomain: string[],
+    range: [number, number],
+    padding: number = 0
+) => {
     assertList(
-        [fullDomain, 'fullDomain is a required argument'],
+        [!!fullDomain, 'fullDomain is a required argument'],
         [Array.isArray(fullDomain), 'fullDomain must be of type array'],
-        [range, 'range is a required argument'],
+        [!!range, 'range is a required argument'],
         [Array.isArray(range), 'range must be of type array']
     )
     return scalePoint().domain(fullDomain).range(range).padding(padding)
 }
 
-const getLinearScale = (fullDomain, range) => {
+const getLinearScale = (fullDomain: number[], range: [number, number]) => {
     assertList(
-        [fullDomain, 'fullDomain is a required argument'],
+        [!!fullDomain, 'fullDomain is a required argument'],
         [Array.isArray(fullDomain), 'fullDomain must be of type array'],
-        [range, 'range is a required argument'],
+        [!!range, 'range is a required argument'],
         [Array.isArray(range), 'range must be of type array']
     )
     return scaleLinear()
@@ -23,11 +27,15 @@ const getLinearScale = (fullDomain, range) => {
         .range(range)
 }
 
-const getBandScale = (fullDomain, range, paddingOuter = 0) => {
+const getBandScale = (
+    fullDomain: string[],
+    range: [number, number],
+    paddingOuter: number = 0
+) => {
     assertList(
-        [fullDomain, 'fullDomain is a required argument'],
+        [!!fullDomain, 'fullDomain is a required argument'],
         [Array.isArray(fullDomain), 'fullDomain must be of type array'],
-        [range, 'range is a required argument'],
+        [!!range, 'range is a required argument'],
         [Array.isArray(range), 'range must be of type array']
     )
     return scaleBand()
